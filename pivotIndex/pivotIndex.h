c4777c9f0289ee20c496ec775446859fee3f28d0
arifinfrds <arifinfrds.engineer@gmail.com>
2023-03-08 14:01:20 +0700

    Initial Commit

---
+++ pivotIndex/pivotIndex.h
@@ -0,0 +1,18 @@
+//
+//  pivotIndex.h
+//  pivotIndex
+//
+//  Created by arifinfrds.engineer on 08/03/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for pivotIndex.
+FOUNDATION_EXPORT double pivotIndexVersionNumber;
+
+//! Project version string for pivotIndex.
+FOUNDATION_EXPORT const unsigned char pivotIndexVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <pivotIndex/PublicHeader.h>
+
+
