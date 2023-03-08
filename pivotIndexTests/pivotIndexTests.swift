e219851644f634ffe5c4765af3573e097c125318
arifinfrds <arifinfrds.engineer@gmail.com>
2023-03-08 14:20:09 +0700

    `pivotIndex` returns zero on edge pivot index

--- pivotIndexTests/pivotIndexTests.swift
+++ pivotIndexTests/pivotIndexTests.swift
@@ -10,7 +10,48 @@ import XCTest
 
 class Solution {
     func pivotIndex(_ nums: [Int]) -> Int {
-        -1
+        
+        if nums.isEmpty {
+            return -1
+        }
+        
+        for (index, number) in nums.enumerated() {
+            
+            let leftSum = calculateLeftSum(beforeIndex: index, source: nums)
+            let rightSum = calculateRightSum(afterIndex: index, source: nums)
+            
+            if leftSum == rightSum {
+                return index
+            }
+        }
+        return -1
+    }
+    
+    // MARK: - Helpers
+    
+    private func calculateLeftSum(beforeIndex index: Int, source nums: [Int]) -> Int {
+        var sum = 0
+        
+        for i in 0..<index {
+            sum += nums[i]
+        }
+        
+        return sum
+    }
+    
+    private func calculateRightSum(afterIndex index: Int, source nums: [Int]) -> Int {
+        
+        guard (index + 1) < nums.count else {
+            return 0
+        }
+        
+        var sum = 0
+        
+        for i in (index + 1)...nums.count - 1 {
+            sum += nums[i]
+        }
+        
+        return sum
     }
 }
 
@@ -30,6 +71,14 @@ final class pivotIndexTests: XCTestCase {
         let result = sut.pivotIndex([1,2,3])
         
         XCTAssertEqual(result, -1)
+    }
+    
+    func test_pivotIndex_returnsZeroOnEdgePivotIndex() {
+        let sut = Solution()
+        
+        let result = sut.pivotIndex([2,1,-1])
+        
+        XCTAssertEqual(result, 0)
     }
 
 }
